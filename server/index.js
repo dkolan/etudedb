f0fefaa1dd4c1c24f6c3617abb160818e7d0b244
dkolan <dan.kolan@gmail.com>
2022-04-29 23:48:06 -0400

    Created POST route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //Middleware
 app.use(cors());
@@ -8,4 +9,34 @@ app.use(express.json());
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
+})
+
+//Routes
+
+//Create an etude
+
+app.post("/etudes" , async (req, res) => {
+    try {
+        const { etude_name,
+                book,
+                composer,
+                etude_key,
+                tempo,
+                range_high,
+                range_low } = req.body
+        
+        const newEtude = await pool.query("INSERT INTO etude (etude_name,"
+                                            + "book, composer, etude_key,"
+                                            + "tempo, range_high, range_low)"
+                                            + "VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+                                            [etude_name, book, composer,
+                                            etude_key, tempo, range_high, range_low]);
+       
+        res.json(newEtude.rows[0]);
+        
+        
+        // console.log(etude_name, book, composer, etude_key, tempo, range_high, range_low);
+    } catch (err) {
+        console.error(err.message)
+    }
 })
